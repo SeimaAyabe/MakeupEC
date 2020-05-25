428b364d3f9b5b72ee51d6961a88a9b660a92a4b
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-05-25 22:10:59 +0900

    パス変数を適用

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World!!";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+
+		int res = 0;
+		for(int i = 1; i <= num; i++) {
+
+			res += i;
+
+		}
+
+		return "total : " + res;
+
 	}
+
 }
