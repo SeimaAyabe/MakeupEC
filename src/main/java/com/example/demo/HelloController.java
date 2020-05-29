4a982cfb3c982cff03aca162ffad3df52409ba95
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-05-30 01:49:07 +0900

    簡単なフォームを作成

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,12 +2,29 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "index";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		// 入力フォームで取り扱うオブジェクトを設定
+		mav.setViewName("index");
+		mav.addObject("msg","お名前を書いて送信してください");
+		// 表示するHTMLを指定
+		return mav;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str, ModelAndView mav) {
+		// 入力フォームで取り扱うオブジェクトを設定
+		mav.setViewName("index");
+		mav.addObject("msg","こんにちは、" + str + "さん！");
+		mav.addObject("value",str);
+		// 表示するHTMLを指定
+		return mav;
 	}
 }
