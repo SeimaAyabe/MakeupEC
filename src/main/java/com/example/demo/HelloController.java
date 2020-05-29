1a0bd1d6f1d1d44f57525391cb586bb6ac6cc2b8
SeimaAyabe <yamakawa.yuya1029@gmail.com>
2020-05-30 00:39:06 +0900

    Thymeleafを導入

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,23 +1,13 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num) {
 
-		int res = 0;
-		for(int i = 1; i <= num; i++) {
-
-			res += i;
-
-		}
-
-		return "total : " + res;
-
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
-
 }
